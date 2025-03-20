206c5f88a7be400f9c7297015474b35aabf2401f
aruna <arunakamakshi09@gmail.com>
2025-03-20 18:10:26 +0530

    Added the footer page

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+        <footer className="bg-gray-100 border-t border-gray-200 py-8 mt-10">
+            <div className="container mx-auto px-4">
+                <div className="flex justify-center items-center">
+                    <p className="text-gray-600 font-medium text-center">© 2025 My App. All Rights Reserved.</p>
+                </div>
+            </div>
+        </footer>
+    );
+}
